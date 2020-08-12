87289d2ff0d7cc9a9e59d47149920f96d9dc0972
Subak Systems <subak@rmb12.local>
2020-08-12 13:58:58 +0900

    2020年 8月12日 水曜日 13時58分58秒 JST

--- index.ts
+++ index.ts
@@ -1,34 +1,48 @@
-import * as typescript from 'typescript'
+import * as ts from 'typescript'
 import * as path from 'path'
 
-const transformer = (_: typescript.Program) => (transformationContext: typescript.TransformationContext) => (sourceFile: typescript.SourceFile) => {
-  function visitNode(node: typescript.Node): typescript.VisitResult<typescript.Node> {
+const replaceName = (name: string) => {
+  if (name.startsWith('.')) {
+    if (name.endsWith('/')) {
+      return `${name}index.js`;
+    } else {
+      return `${name}.js`;
+    }
+  } else {
+    return `/pkit-ts/esm/${name}/index.js`;
+  }
+}
+
+const transformer = (_: ts.Program) => (transformationContext: ts.TransformationContext) => (sourceFile: ts.SourceFile) => {
+  function visitNode(node: ts.Node): ts.VisitResult<ts.Node> {
     if (shouldMutateModuleSpecifier(node)) {
-      if (typescript.isImportDeclaration(node)) {
-        const newModuleSpecifier = typescript.createLiteral(`${node.moduleSpecifier.text}.js`)
-        return typescript.updateImportDeclaration(node, node.decorators, node.modifiers, node.importClause, newModuleSpecifier)
-      } else if (typescript.isExportDeclaration(node)) {
-        const newModuleSpecifier = typescript.createLiteral(`${node.moduleSpecifier.text}.js`)
-        return typescript.updateExportDeclaration(node, node.decorators, node.modifiers, node.exportClause, newModuleSpecifier, false)
+
+      const newModuleSpecifier = ts.createLiteral(replaceName(node.moduleSpecifier.text));
+      // const newModuleSpecifier = ts.createLiteral(`${node.moduleSpecifier.text}.js`);
+
+      if (ts.isImportDeclaration(node)) {
+        return ts.updateImportDeclaration(node, node.decorators, node.modifiers, node.importClause, newModuleSpecifier)
+      } else if (ts.isExportDeclaration(node)) {
+        return ts.updateExportDeclaration(node, node.decorators, node.modifiers, node.exportClause, newModuleSpecifier, false)
       }
     }
 
-    return typescript.visitEachChild(node, visitNode, transformationContext)
+    return ts.visitEachChild(node, visitNode, transformationContext)
   }
 
-  function shouldMutateModuleSpecifier(node: typescript.Node): node is (typescript.ImportDeclaration | typescript.ExportDeclaration) & { moduleSpecifier: typescript.StringLiteral } {
-    if (!typescript.isImportDeclaration(node) && !typescript.isExportDeclaration(node)) return false
+  function shouldMutateModuleSpecifier(node: ts.Node): node is (ts.ImportDeclaration | ts.ExportDeclaration) & { moduleSpecifier: ts.StringLiteral } {
+    if (!ts.isImportDeclaration(node) && !ts.isExportDeclaration(node)) return false
     if (node.moduleSpecifier === undefined) return false
     // only when module specifier is valid
-    if (!typescript.isStringLiteral(node.moduleSpecifier)) return false
+    if (!ts.isStringLiteral(node.moduleSpecifier)) return false
     // only when path is relative
-    if (!node.moduleSpecifier.text.startsWith('./') && !node.moduleSpecifier.text.startsWith('../')) return false
+    // if (!node.moduleSpecifier.text.startsWith('./') && !node.moduleSpecifier.text.startsWith('../')) return false
     // only when module specifier has no extension
     if (path.extname(node.moduleSpecifier.text) !== '') return false
     return true
   }
 
-  return typescript.visitNode(sourceFile, visitNode)
+  return ts.visitNode(sourceFile, visitNode)
 }
 
 export default transformer
